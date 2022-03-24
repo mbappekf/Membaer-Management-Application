e72321db43884ecb966cab4bc3ba0781aa3ff64b
mbappekf <kaname.f.1995g@gmail.com>
2022-03-24 23:56:30 +0900

    commit 2nd

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -26,5 +26,5 @@ CREATE TABLE IF NOT EXISTS t_salary(
     user_id VARCHAR(50),
     year_mnt VARCHAR(50),
     salary INT,
-    PRIMARY KEY(user_id, year_month)
+    PRIMARY KEY(user_id, year_mnt)
 );
